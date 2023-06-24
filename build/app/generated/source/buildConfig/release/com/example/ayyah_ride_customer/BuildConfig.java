cf1f9656f8bd6d01401defeb8678a79c9b08b2be
mainerprenuer <moustymainer@gmail.com>
2023-06-24 18:32:04 +0100

    all touches on AyyarRide second push

---
+++ build/app/generated/source/buildConfig/release/com/example/ayyah_ride_customer/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.ayyah_ride_customer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.ayyah_ride_customer";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
